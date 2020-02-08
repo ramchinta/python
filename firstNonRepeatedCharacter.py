bb616ec918313f41225b5335b4c62756f5f353c1
Lakshman Chinta <mithrachinta@gmail.com>
2020-02-08 12:14:14 -0500

    First non repeated character of a string

---
+++ firstNonRepeatedCharacter.py
@@ -0,0 +1,15 @@
+string = input('Enter String: ')
+numOfChar = 256
+def getCharCount(string):
+    print(string)
+    count = [0] * numOfChar
+    for i in string:
+        count[ord(i)] = count[ord(i)] + 1
+
+    for i in string:
+        if count[ord(i)] == 1:
+            print('The first non repeated character is '+i)
+            return
+    print('All the characters in the string are repeated')
+
+getCharCount(string)
