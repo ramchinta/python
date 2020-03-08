6ea0386f5736e4ea117b4f515480c0353e458acd
Lakshman Chinta <mithrachinta@gmail.com>
2020-03-07 23:28:05 -0500

    Search a 2D matrix

---
+++ Search a 2D matrix.py
@@ -0,0 +1,54 @@
+'''Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.'''
+
+
+class Solution:
+    def __init__(self):
+        pass
+    def searchMatrix(self, matrix, target):
+        """
+                :type matrix: List[List[int]]
+                :type target: int
+                :rtype: bool
+                """
+        # an empty matrix obviously does not contain `target` (make this check
+        # because we want to cache `width` for efficiency's sake)
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+
+        # cache these, as they won't change.
+        height = len(matrix)
+        width = len(matrix[0])
+
+        # start our "pointer" in the bottom-left
+        row = height - 1
+        col = 0
+
+        while col < width and row >= 0:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else:  # found it
+                return True
+
+        return False
+
+print(Solution().searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],5))
+#True
