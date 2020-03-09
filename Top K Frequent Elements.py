f3da35b23d8da046e0007a9cd0a22f25d446ffbc
Lakshman Chinta <mithrachinta@gmail.com>
2020-03-08 23:02:25 -0400

    Top K Frequent Words

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -1,3 +1,18 @@
+'''Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.'''
+
 from collections import Counter
 
 
@@ -14,3 +29,4 @@ class Solution(object):
         return [element for element, count in k_most_common]  # [1, 2]
 
 print(Solution().topKFrequent([1,1,1,2,2,3],2))
+#[1, 2]
